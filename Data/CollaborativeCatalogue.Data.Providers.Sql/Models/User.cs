9a0313693ad1a9b2c04eb685263d9bfc149d8de4
Sébastien Fischer <93124650+Thorngar@users.noreply.github.com>
2022-06-24 17:24:37 +0200

    Adding UsersController

--- Data/CollaborativeCatalogue.Data.Providers.Sql/Models/User.cs
+++ Data/CollaborativeCatalogue.Data.Providers.Sql/Models/User.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollaborativeCatalogue.Data.Providers.Sql.Models
 {
-    [Table("User", Schema="dbo")]
+    [Table("User", Schema = "dbo")]
     public class User
     {
         [Key]
---
+++ Presentation/CollaborativeCatalogue.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using CollaborativeCatalogue.Data.Providers.Sql;
+using CollaborativeCatalogue.Data.Providers.Sql.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CollaborativeCatalogue.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly CollaborativeCatalogueDbContext collaborativeCatalogueDbContext;
+
+        public UsersController(CollaborativeCatalogueDbContext collaborativeCatalogueDbContext)
+        {
+            this.collaborativeCatalogueDbContext = collaborativeCatalogueDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllAsync()
+        {
+            return Ok(await collaborativeCatalogueDbContext.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetByIdAsync(int id)
+        {
+            return Ok(await collaborativeCatalogueDbContext.Users.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateAsync([FromBody] User user)
+        {
+            collaborativeCatalogueDbContext.Attach(user);
+            await collaborativeCatalogueDbContext.SaveChangesAsync();
+            return Created("", user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] User user)
+        {
+            var dbUser = await collaborativeCatalogueDbContext.Users.FindAsync(id);
+
+            if (dbUser == null)
+            {
+                return NotFound();
+            }
+
+            collaborativeCatalogueDbContext.Attach(user);
+            await collaborativeCatalogueDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var dbUser = await collaborativeCatalogueDbContext.Users.FindAsync(id);
+
+            if (dbUser == null)
+            {
+                return NotFound();
+            }
+
+            collaborativeCatalogueDbContext.Remove(dbUser);
+            await collaborativeCatalogueDbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}

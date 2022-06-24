33e240b687f5506679126193d2db5736bc9c8615
Sébastien Fischer <93124650+Thorngar@users.noreply.github.com>
2022-06-24 17:47:30 +0200

    Adding params to classes

--- Data/CollaborativeCatalogue.Data.Providers.Sql/Models/EducationTool.cs
+++ Data/CollaborativeCatalogue.Data.Providers.Sql/Models/EducationTool.cs
@@ -9,7 +9,11 @@ namespace CollaborativeCatalogue.Data.Providers.Sql.Models
     public class EducationTool
     {
         public int Id { get; set; }
+        public string Description { get; set; }
+        public string ToolType { get; set; }
         public int MinAge { get; set; }
         public int MaxAge { get; set; }
+        public int Duration { get; set; }
+        public double Price { get; set; }
     }
 }

3858adf6e5fa7c82691f19aeb5f304e6b1549ea6
Sébastien Fischer <93124650+Thorngar@users.noreply.github.com>
2022-06-25 11:53:51 +0200

    User adjustments

--- Data/CollaborativeCatalogue.Data.Providers.Sql/Models/EducationTool.cs
+++ Data/CollaborativeCatalogue.Data.Providers.Sql/Models/EducationTool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace CollaborativeCatalogue.Data.Providers.Sql.Models
 {
     public class EducationTool
     {
+        [Key]
         public int Id { get; set; }
         public string Description { get; set; }
         public string ToolType { get; set; }

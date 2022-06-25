aaaaa75e17dbf8f3f66cf0f24621eea046d296a3
Sébastien Fischer <93124650+Thorngar@users.noreply.github.com>
2022-06-25 10:48:36 +0200

    Adding DB with right names

---
+++ Database/Database/Scripts/ThematicsInsert.sql
@@ -0,0 +1,14 @@
+﻿USE CollaborativeCatalogue;
+
+INSERT INTO Thematics VALUES (1, 'Agriculture et alimentation');
+INSERT INTO Thematics VALUES (2, 'Commerce équitable');
+INSERT INTO Thematics VALUES (3, 'Droits de l’enfant et droits humains');
+INSERT INTO Thematics VALUES (4, 'Développement durable');
+INSERT INTO Thematics VALUES (5, 'Éducation à la diversité');
+INSERT INTO Thematics VALUES (6, 'Environnement, eau et climat');
+INSERT INTO Thematics VALUES (7, 'Genre');
+INSERT INTO Thematics VALUES (8, 'Conflits et paix');
+INSERT INTO Thematics VALUES (9, 'Handicap');
+INSERT INTO Thematics VALUES (10, 'Migrations et réfugiés');
+INSERT INTO Thematics VALUES (11, 'Mondialisation');
+INSERT INTO Thematics VALUES (12, 'Valeurs');

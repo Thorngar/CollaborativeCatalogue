64d1befc9c04e96e304d65f876d690d9ef08c37f
Sébastien Fischer <93124650+Thorngar@users.noreply.github.com>
2022-06-25 10:55:07 +0200

    Thematics Script

--- Database/Database/Scripts/ThematicsInsert.sql
+++ Database/Database/Scripts/ThematicsInsert.sql
@@ -1,14 +1,14 @@
 ﻿USE CollaborativeCatalogue;
 
-INSERT INTO Thematics VALUES (1, 'Agriculture et alimentation');
-INSERT INTO Thematics VALUES (2, 'Commerce équitable');
-INSERT INTO Thematics VALUES (3, 'Droits de l’enfant et droits humains');
-INSERT INTO Thematics VALUES (4, 'Développement durable');
-INSERT INTO Thematics VALUES (5, 'Éducation à la diversité');
-INSERT INTO Thematics VALUES (6, 'Environnement, eau et climat');
-INSERT INTO Thematics VALUES (7, 'Genre');
-INSERT INTO Thematics VALUES (8, 'Conflits et paix');
-INSERT INTO Thematics VALUES (9, 'Handicap');
-INSERT INTO Thematics VALUES (10, 'Migrations et réfugiés');
-INSERT INTO Thematics VALUES (11, 'Mondialisation');
-INSERT INTO Thematics VALUES (12, 'Valeurs');
+INSERT INTO Thematics VALUES ('Agriculture et alimentation');
+INSERT INTO Thematics VALUES ('Commerce équitable');
+INSERT INTO Thematics VALUES ('Droits de l’enfant et droits humains');
+INSERT INTO Thematics VALUES ('Développement durable');
+INSERT INTO Thematics VALUES ('Éducation à la diversité');
+INSERT INTO Thematics VALUES ('Environnement, eau et climat');
+INSERT INTO Thematics VALUES ('Genre');
+INSERT INTO Thematics VALUES ('Conflits et paix');
+INSERT INTO Thematics VALUES ('Handicap');
+INSERT INTO Thematics VALUES ('Migrations et réfugiés');
+INSERT INTO Thematics VALUES ('Mondialisation');
+INSERT INTO Thematics VALUES ('Valeurs');

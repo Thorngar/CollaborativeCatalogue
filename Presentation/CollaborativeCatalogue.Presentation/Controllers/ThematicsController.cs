6274dc0ed362f97b4bc694488a3165bb98152aea
Sébastien Fischer <93124650+Thorngar@users.noreply.github.com>
2022-06-26 00:34:10 +0200

    Thematics verifications

--- Presentation/CollaborativeCatalogue.Presentation/Controllers/ThematicsController.cs
+++ Presentation/CollaborativeCatalogue.Presentation/Controllers/ThematicsController.cs
@@ -2,10 +2,11 @@
 using CollaborativeCatalogue.Data.Providers.Sql.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.Entity;
+using System.Security.Claims;
 
 namespace CollaborativeCatalogue.Presentation.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ThematicsController : ControllerBase
     {
@@ -31,39 +32,58 @@ namespace CollaborativeCatalogue.Presentation.Controllers
         [HttpPost]
         public async Task<ActionResult<Thematic>> CreateAsync([FromBody] Thematic thematic)
         {
-            collaborativeCatalogueDbContext.Attach(thematic);
-            await collaborativeCatalogueDbContext.SaveChangesAsync();
-            return Created("", thematic);
-        }
+            CurrentUser currentUser = this.GetCurrentUser();
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAsync(int id, [FromBody] Thematic thematic)
-        {
-            var dbTheme = await collaborativeCatalogueDbContext.Thematics.FindAsync(id);
-
-            if (dbTheme == null)
+            try
             {
-                return NotFound();
-            }
+                if (currentUser.RoleId == 1)
+                {
+                    collaborativeCatalogueDbContext.Attach(thematic);
+                    await collaborativeCatalogueDbContext.SaveChangesAsync();
 
-            collaborativeCatalogueDbContext.Attach(thematic);
-            await collaborativeCatalogueDbContext.SaveChangesAsync();
-            return Ok();
+                    return Created("", thematic);
+                }
+
+                return Unauthorized();
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete]
         public async Task<IActionResult> DeleteAsync(int id)
         {
-            var dbTheme = await collaborativeCatalogueDbContext.Thematics.FindAsync(id);
+            CurrentUser currentUser = this.GetCurrentUser();
 
-            if (dbTheme == null)
+            if (currentUser.RoleId == 1)
             {
-                return NotFound();
+                var dbTheme = await collaborativeCatalogueDbContext.Thematics.FindAsync(id);
+
+                collaborativeCatalogueDbContext.Remove(dbTheme);
+                await collaborativeCatalogueDbContext.SaveChangesAsync();
+                return Ok();
             }
 
-            collaborativeCatalogueDbContext.Remove(dbTheme);
-            await collaborativeCatalogueDbContext.SaveChangesAsync();
-            return Ok();
+            return Unauthorized();
+        }
+
+        private CurrentUser GetCurrentUser()
+        {
+            var context = this.HttpContext.User.Claims;
+            CurrentUser currentUser = new CurrentUser();
+
+            if (context.Count() != 0)
+            {
+                currentUser.Id = Int32.Parse(context.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+                currentUser.Email = context.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+                currentUser.RoleId = Int32.Parse(context.FirstOrDefault(r => r.Type == ClaimTypes.Role).Value);
+
+            }
+
+            return currentUser;
         }
     }
 }

527c5e6c82c9ecf613e2a9c5a68dd6b98f8e595d
LaynMDream <layn_mdream_@hotmail.com>
2022-09-24 11:35:00 +0200

    up

--- Presentation/iledepaix-main/src/services/user.service.js
+++ Presentation/iledepaix-main/src/services/user.service.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import authHeader from './auth-header';
-const API_URL = 'http://localhost:8080/api/test/';
+import axios from "axios";
+import authHeader from "./auth-header";
+const API_URL = "https://localhost:7015/api/users/";
 class UserService {
   getUser() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
+    return axios.get(API_URL + "user", { headers: authHeader() });
   }
 }
 export default new UserService();
